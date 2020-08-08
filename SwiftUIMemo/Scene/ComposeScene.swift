579ce21026152e700231a82d0b8618738df2bf1d
fogbar <kimdongjun6570@gmail.com>
2020-08-09 00:07:31 +0900

    compose scene ui

---
+++ SwiftUIMemo/Scene/ComposeScene.swift
@@ -0,0 +1,54 @@
+//
+//  ComposeScene.swift
+//  SwiftUIMemo
+//
+//  Created by garlic on 2020/08/08.
+//  Copyright © 2020 com.codershigh. All rights reserved.
+//
+
+import SwiftUI
+
+struct ComposeScene: View {
+    @EnvironmentObject var memoStore:MemoStore
+    @State private var content:String = ""
+    
+    var body: some View {
+        NavigationView {
+            VStack {
+                //$ 문자를 사용하면 content 속성과 textField가 바인딩 되고 textField에 문자를 입력하면 content 속성에 자동으로 저장됨
+                //반대로 content 속성에 문자열을 저장하면 TextField에도 동일한 문자열이 표시됨
+                TextField("", text: $content)
+            }
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+            .navigationBarTitle("새 메모", displayMode: .inline)
+        .navigationBarItems(leading: DismissButton(), trailing: SaveButton())
+        }
+    }
+}
+
+fileprivate struct DismissButton:View {
+    var body: some View {
+        Button(action: {
+            
+        }, label: {
+            Text("취소")
+        })
+    }
+}
+
+fileprivate struct SaveButton:View {
+    var body: some View {
+        Button(action: {
+            
+        }, label: {
+            Text("저장")
+        })
+    }
+}
+
+struct ComposeScene_Previews: PreviewProvider {
+    static var previews: some View {
+        ComposeScene()
+        .environmentObject(MemoStore())
+    }
+}
