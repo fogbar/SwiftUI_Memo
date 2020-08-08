3d9493b1cdae89d89a75a3d8d259495646674b75
fogbar <kimdongjun6570@gmail.com>
2020-08-09 00:34:04 +0900

    list -> Compose

--- SwiftUIMemo/Scene/MemoListScene.swift
+++ SwiftUIMemo/Scene/MemoListScene.swift
@@ -13,6 +13,8 @@ struct MemoListScene: View {
     //이렇게 함으로서 뷰가 생성되는 시점에 공유 데이터 목록을 확인하고 store 객체와 동일한 형식을 가진 객체가 등록이 되어 있다면 여기에 자동으로 저장해줌. -> SwiftUI에선 이런 방식으로 하나의 데이터를 여러 뷰에서 구현
     @EnvironmentObject var store: MemoStore
     @EnvironmentObject var formatter:DateFormatter
+    //ComposeScene을 제어할때 사용하는 변수
+    @State private var showComposer:Bool = false
     
     var body: some View {
         //네비게이션 만드는 법. -> NavigationView를 추가해주면 상단에 NavigationBar가 추가됨.
@@ -26,8 +28,30 @@ struct MemoListScene: View {
                 MemoCell(memo: memo)
             }
                 //Swift에서는 method라고 부르지만 SwiftUI에서는 이렇게 .찍고 사용하는 Method 들을 modifier라고 부름
-        .navigationBarTitle("내 메모")
+            .navigationBarTitle("내 메모")
+                //showComposer 앞에 $문자를 붙여야 값이 복사되는 것이 아니라 binding이 전달되는데 여기서 전달한 속성은 ModalButton의 show 속성에 저장됨
+                //뷰 외부의 속성을 바꾸고 싶다면 이렇게 binding 형태로 전달해야 함
+                //Parameter로 전달시에 $ 문자를 붙이고, 전달 받는 쪽에서는 @Binding 형식으로 선언하는 패턴
+            .navigationBarItems(trailing: ModalButton(show: $showComposer))
+                //showComposer 값이 true이면 content에 있는 클로져를 실행하여 ComposeScene 인스턴스를 모달 형식으로 리턴함
+            .sheet(isPresented: $showComposer, content: {
+                //preview에서 다시 눌러도 반응하지 않는 이유는 XCode preview 에러임
+                ComposeScene(showComposer: self.$showComposer)
+            })
         }
+    }
+}
+
+fileprivate struct ModalButton:View {
+    //다른 곳에 있는 값을 바꾸기 위한 속성
+    @Binding var show: Bool
+    
+    var body: some View {
+        Button(action: {
+            self.show = true
+        }, label: {
+            Image(systemName: "plus")
+        })
     }
 }
 
